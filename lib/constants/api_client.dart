0f7eae6ab26e08df12ed67d3d974704aa88df0fc
Opeyemi Noah <Devlonoah@gmail.com>
2022-12-13 09:01:28 +0100

    removed unneeded slash

--- lib/constants/api_client.dart
+++ lib/constants/api_client.dart
@@ -1,5 +1,5 @@
 class ApiClient {
   static String baseUrl =
-      "https://raw.githubusercontent.com/web3author/web3labsforall/main/";
+      "https://raw.githubusercontent.com/web3author/web3labsforall/main";
   static String labList = "$baseUrl/lab_list.json";
 }
